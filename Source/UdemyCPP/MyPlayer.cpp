e23b06176ba6848ce9c5e9074bc01d4d5d7a5fa6
Luben Lubenov <65513534+Moonriderx@users.noreply.github.com>
2022-03-05 13:04:33 +0200

    Basic Pawn Functionalities

--- Source/UdemyCPP/MyPlayer.cpp
+++ Source/UdemyCPP/MyPlayer.cpp
@@ -4,9 +4,10 @@
 #include "MyPlayer.h"
 #include "Components/StaticMeshComponent.h"
 #include "Camera/CameraComponent.h"
+#include "Components/InputComponent.h"
 
 // Sets default values
-AMyPlayer::AMyPlayer()
+AMyPlayer::AMyPlayer() 
 {
  	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -22,6 +23,9 @@ AMyPlayer::AMyPlayer()
 	Camera->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
 
 	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+	CurrentVelocity = FVector(0.f);
+	MaxSpeed = 100.f;
 }
 
 // Called when the game starts or when spawned
@@ -36,6 +40,9 @@ void AMyPlayer::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+	SetActorLocation(NewLocation);
+
 }
 
 // Called to bind functionality to input
@@ -43,5 +50,18 @@ void AMyPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	PlayerInputComponent->BindAxis(TEXT("MoveForward"), this, &AMyPlayer::MoveForward);
+	PlayerInputComponent->BindAxis(TEXT("MoveRight"), this, &AMyPlayer::MoveRight);
+
+}
+
+void AMyPlayer::MoveForward(float Value)
+{
+	CurrentVelocity.X = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
+}
+
+void AMyPlayer::MoveRight(float Value)
+{
+	CurrentVelocity.Y = FMath::Clamp(Value, -1.f, 1.f) * MaxSpeed;
 }
 
