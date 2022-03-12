07ea2fa2aa88f6654a28e375d48f40312f681a82
Luben Lubenov <65513534+Moonriderx@users.noreply.github.com>
2022-03-12 20:31:23 +0200

    Add files via upload

--- Source/UdemyCPP/MainCharacter.cpp
+++ Source/UdemyCPP/MainCharacter.cpp
@@ -2,12 +2,31 @@
 
 
 #include "MainCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
 // Sets default values
 AMainCharacter::AMainCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	/** Create Camera Boom (pulls towards the player if there is a collision) */
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetRootComponent());
+	CameraBoom->TargetArmLength = 600.f; // Camera Follows at this distance
+	CameraBoom->bUsePawnControlRotation = true; // Rotate arm based on controller
+
+	/** Create Follow Camera */
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("Follow Camera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	/** Attach the camera to the end of the boom and let the boom adjust to match 
+	the controller orientation */
+	FollowCamera->bUsePawnControlRotation = false;
+
+	// Set Our turn rates for input
+	BaseTurnRate = 65.f;
+	BaseLookUpRate = 65.f;
 
 }
 
@@ -29,6 +48,35 @@ void AMainCharacter::Tick(float DeltaTime)
 void AMainCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}	
+
+void AMainCharacter::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f)) 
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		// create a FRotationMatrix from YawRotation.	
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+    }
+}
+
+void AMainCharacter::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
+
+		// create a FRotationMatrix from YawRotation.	
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(Direction, Value);
+	}
 
 }
 
--- Source/UdemyCPP/MainCharacter.h
+++ Source/UdemyCPP/MainCharacter.h
@@ -6,6 +6,7 @@
 #include "GameFramework/Character.h"
 #include "MainCharacter.generated.h"
 
+
 UCLASS()
 class UDEMYCPP_API AMainCharacter : public ACharacter
 {
@@ -14,6 +15,20 @@ class UDEMYCPP_API AMainCharacter : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AMainCharacter();
+
+	/** Camera boom positioning the camera behind the player */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* CameraBoom;
+
+	/** Follow Camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* FollowCamera; 
+
+	/** Base turn rate to scale turning functions for the camera */
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseTurnRate;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
+	float BaseLookUpRate;
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,5 +40,11 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	/** Called for forwards/backwards input */
+	void MoveForward(float Value);
+
+	/** Called for side to side input */
+	void MoveRight(float Value);
 
 };
