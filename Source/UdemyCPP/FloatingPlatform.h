5d5f8d4c5fb5ee03542bc6fd1c4820bccc3c6695
Luben Lubenov <65513534+Moonriderx@users.noreply.github.com>
2022-03-20 11:48:45 +0200

    Added Floating Platforms Functionality

---
+++ Source/UdemyCPP/FloatingPlatform.h
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FloatingPlatform.generated.h"
+
+
+UCLASS()
+class UDEMYCPP_API AFloatingPlatform : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFloatingPlatform();
+
+	/** Mesh for the Moving Platform */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Platform")
+	class UStaticMeshComponent* Platform;
+
+	UPROPERTY(EditAnywhere)
+	FVector StartPoint;
+
+	UPROPERTY(EditAnywhere, meta = (MakeEditWidget = "true"))
+	FVector EndPoint;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	float InterpSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	float InterpTime;
+
+	FTimerHandle InterpTimer;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Platform")
+	bool bInterping;
+
+	float Distance;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void ToggleInterping();
+	
+	// we can change the parameters passed in since we've made them references
+	void SwapVectors(FVector& VecOne, FVector& VecTwo);
+
+};
