e99ac9adfa56ca94553c8262e282c2abe985a2dd
Luben Lubenov <65513534+Moonriderx@users.noreply.github.com>
2022-08-27 19:38:12 +0300

    Added more logic in the Enemy.cpp

--- Source/UdemyCPP/Enemy.cpp
+++ Source/UdemyCPP/Enemy.cpp
@@ -5,6 +5,7 @@
 #include "Components/SphereComponent.h"
 #include "AIController.h"
 #include "MainCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 
 // Sets default values
@@ -20,6 +21,8 @@ AEnemy::AEnemy()
 	CombatSphere = CreateDefaultSubobject<USphereComponent>(TEXT("CombatSphere"));
 	CombatSphere->SetupAttachment(GetRootComponent());
 	CombatSphere->InitSphereRadius(75.f);
+
+	bOverlappingCombatSphere = false;
 
 }
 
@@ -65,15 +68,48 @@ void AEnemy::AggroSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
 }
 void AEnemy::AggroSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-
+	if (OtherActor)
+	{
+		AMainCharacter* Main = Cast<AMainCharacter>(OtherActor);
+		if (Main)
+		{
+			SetEnemyMovementStatus(EEnemyMovementStatus::EMS_Idle);
+			if (AIController)
+			{
+				AIController->StopMovement();
+			}
+			
+		}
+	}
 }
 void AEnemy::CombatSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-
+	if (OtherActor)
+	{
+		AMainCharacter* Main = Cast<AMainCharacter>(OtherActor);
+		if (Main)
+		{
+			CombatTarget = Main;
+			bOverlappingCombatSphere = true;
+			SetEnemyMovementStatus(EEnemyMovementStatus::EMS_Attacking);
+		}
+	}
 }
 void AEnemy::CombatSphereOnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
 {
-
+	if (OtherActor)
+	{
+		AMainCharacter* Main = Cast<AMainCharacter>(OtherActor);
+		if (Main)
+		{
+			bOverlappingCombatSphere = false;
+			if (EnemyMovementStatus != EEnemyMovementStatus::EMS_Attacking) // for not attacking, call move to target, if we are attacking, don't call -> call it from the AnimBP as soon as the attack animation is finished
+			{
+				MoveToTarget(Main);
+				CombatTarget = nullptr;
+			}
+		}
+	}
 }
 
 void AEnemy::MoveToTarget(class AMainCharacter* Target)
@@ -84,11 +120,25 @@ void AEnemy::MoveToTarget(class AMainCharacter* Target)
 	{
 		FAIMoveRequest MoveRequest;
 		MoveRequest.SetGoalActor(Target);
-		MoveRequest.SetAcceptanceRadius(5.0f);
+		MoveRequest.SetAcceptanceRadius(10.0f);
 
 		FNavPathSharedPtr NavPath;
 
 		AIController->MoveTo(MoveRequest, &NavPath);
+
+
+		/**
+		* 
+		TArray<FNavPathPoint> PathPoints = NavPath->GetPathPoints(); // it will return TArray of FNavPathPoints 
+
+		for (auto Point : PathPoints) // it will give us a point which is a element in an array called PathPoints
+		{
+			FVector Location = Point.Location;
+
+			UKismetSystemLibrary::DrawDebugSphere(this, Location, 25.f, 8, FLinearColor::Green, 10.f, 0.5f);
+		}
+
+		*/
 
 	}
 }
